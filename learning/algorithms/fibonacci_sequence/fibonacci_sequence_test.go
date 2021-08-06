2521f741ab89b52bac4778e4d4be453473cc96e0
Roman.Erema <roman.erema@itechart-group.com>
2021-08-06 14:57:48 +0300

    Fibonacci Sequence

---
+++ learning/algorithms/fibonacci_sequence/fibonacci_sequence_test.go
@@ -0,0 +1,93 @@
+package fibonaccisequence_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFibonacci(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		n, want int
+	}{
+		{
+			name: "Case 0",
+			n:    3,
+			want: 2,
+		},
+		{
+			name: "Case 1",
+			n:    10,
+			want: 55,
+		},
+		{
+			name: "Case 2",
+			n:    0,
+			want: 0,
+		},
+		{
+			name: "Case 3",
+			n:    30,
+			want: 832040,
+		},
+	}
+
+	for _, tt := range tests {
+		testCase := tt
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, testCase.want, fibonacciRecursive(testCase.n))
+			assert.Equal(t, testCase.want, fibonacciCache(testCase.n))
+			assert.Equal(t, testCase.want, fibonacciIterative(testCase.n))
+		})
+	}
+}
+
+const fibPrev, fibNext = 1, 2
+
+// Time O(N^2), since we recount numbers in recursion
+// Space O(N), since we use stretch recursion stack.
+func fibonacciRecursive(number int) int {
+	if number <= 1 {
+		return number
+	}
+
+	return fibonacciRecursive(number-fibPrev) + fibonacciRecursive(number-fibNext)
+}
+
+// Time O(N), since we iterate input one time and get counted result from cache
+// Space O(N), since we involve map as a cache which is equal to input length.
+func fibonacciCache(number int) int {
+	if number <= 1 {
+		return number
+	}
+
+	return cacheHelper(number, map[int]int{0: 0, 1: 1})
+}
+
+func cacheHelper(number int, cache map[int]int) int {
+	if _, ok := cache[number]; !ok {
+		cache[number] = cacheHelper(number-fibPrev, cache) + cacheHelper(number-fibNext, cache)
+	}
+
+	return cache[number]
+}
+
+// Time O(N), since we iterate input one time
+// Space O(1), since we don't involve any additional data structure.
+func fibonacciIterative(number int) int {
+	if number <= 1 {
+		return number
+	}
+
+	prev1, prev2 := 0, 1
+	for i := 2; i <= number; i++ {
+		prev1, prev2 = prev2, prev1+prev2
+	}
+
+	return prev2
+}
