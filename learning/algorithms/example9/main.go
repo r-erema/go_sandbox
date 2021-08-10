ccdc7bf0769740cf408b995420abbc3c80f2d77e
Roman.Erema <roman.erema@itechart-group.com>
2021-08-10 20:26:07 +0300

    N Largest Elements

---
+++ learning/algorithms/example9/main.go
@@ -0,0 +1,29 @@
+package example9
+
+/*
+	Average, Worst: O(n log m) time | O(1) space
+*/
+func NLargestElements(array []int, n int) (result []int) {
+	result = make([]int, n)
+	lastIndex := n - 1
+
+	shift := func(shiftEndIndex int, elementToUpdate int) {
+		for j := 0; j < shiftEndIndex; j++ {
+			result[j] = result[j+1]
+		}
+
+		result[shiftEndIndex] = elementToUpdate
+	}
+
+	for _, element := range array {
+		for i := lastIndex; i >= 0; i-- {
+			if element > result[i] {
+				shift(i, element)
+
+				break
+			}
+		}
+	}
+
+	return result
+}
