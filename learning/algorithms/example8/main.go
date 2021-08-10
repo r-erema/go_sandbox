53da230fa304906d368b57c4a1edc293f701cb0b
Roman.Erema <roman.erema@itechart-group.com>
2021-08-10 18:39:06 +0300

    Binary Search

---
+++ learning/algorithms/example8/main.go
@@ -0,0 +1,23 @@
+package example8
+
+/*
+	Average, Worst: O(log n) time | O(1) space
+*/
+func BinarySearch(array []int, needle int) int {
+	leftPointer, rightPointer := 0, len(array)-1
+
+	for leftPointer <= rightPointer {
+		cutPoint := (leftPointer + rightPointer) / 2
+		potentialResult := array[cutPoint]
+
+		if potentialResult > needle {
+			rightPointer = cutPoint - 1
+		} else if potentialResult < needle {
+			leftPointer = cutPoint + 1
+		} else {
+			return cutPoint
+		}
+	}
+
+	return -1
+}
