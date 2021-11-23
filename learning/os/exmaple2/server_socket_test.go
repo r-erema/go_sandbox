686c968834fd8fac8815fccf5d5ef239bd3e27c1
Roman.Erema <roman.erema@itechart-group.com>
2021-11-23 23:52:14 +0300

    Server on sockets

---
+++ learning/os/exmaple2/server_socket_test.go
@@ -0,0 +1,33 @@
+package exmaple2_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/r-erema/go_sendbox/learning/os/exmaple2"
+	"github.com/reiver/go-telnet"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServer(t *testing.T) {
+	t.Parallel()
+
+	serverIncomingData := make(chan []byte)
+
+	go func() {
+		err := exmaple2.Server("127.0.0.1", 7777, serverIncomingData)
+		require.NoError(t, err)
+	}()
+
+	time.Sleep(time.Millisecond * 100)
+
+	conn, err := telnet.DialTo("127.0.0.1:7777")
+	require.NoError(t, err)
+
+	data := []byte("TEST DATA")
+	_, err = conn.Write(data)
+	require.NoError(t, err)
+
+	assert.Equal(t, data, <-serverIncomingData)
+}
