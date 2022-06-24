3ef6b261ca60efbc3308c382cd3e715a93fa632b
Roman.Erema <roman.erema@itechart-group.com>
2022-06-24 13:18:34 +0300

    K8S controller-runtime example

---
+++ learning/k8s/example3/codegen/update-codegen.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(ls -d -1 "${GOPATH}"/pkg/mod/k8s.io/code-generator@v0.23.5 2>/dev/null || echo ../code-generator)}
+MODULE=github.com/r-erema/go_sendbox
+
+rm -rf "${GOPATH}/src/${MODULE}"
+
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  ${MODULE}/learning/k8s/example3/pkg/generated \
+  ${MODULE}/learning/k8s/example3/pkg/apis \
+  solarsystem:v1 \
+  --go-header-file "${SCRIPT_ROOT}"/codegen/boilerplate.go.txt
+
+cp -r "${GOPATH}/src/${MODULE}"/learning/k8s/example3/pkg/apis/solarsystem/v1 \
+      "$(dirname "${BASH_SOURCE[0]}")"/../pkg/apis/solarsystem
+
+cp -r "${GOPATH}/src/${MODULE}"/learning/k8s/example3/pkg/generated \
+      "$(dirname "${BASH_SOURCE[0]}")"/../pkg
