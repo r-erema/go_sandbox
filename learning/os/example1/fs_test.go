a7a0494c591170184102b37ce8e30d495f25f638
roman <roma.a.erema@gmail.com>
2023-09-07 08:17:46 +0300

    Problem: Daily Temperatures

--- learning/os/example1/fs_test.go
+++ learning/os/example1/fs_test.go
@@ -2,61 +2,21 @@ package example1_test
 
 import (
 	"encoding/json"
+	"github.com/r-erema/go_sendbox/utils/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
 	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"syscall"
 	"testing"
-
-	"github.com/r-erema/go_sendbox/utils/test"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 const testFSSourcePath = "./test_data/test_fs_source"
 
-func TestChroot(t *testing.T) {
-	t.Parallel()
-
-	cmd := exec.Command("./test_data/test_bin_source/chroot", "./test_data/test_fs_source")
-	output, err := cmd.Output()
-	require.NoError(t, err)
-
-	resp := new(struct {
-		DirsBeforeChroot []string `json:"dirs_before_chroot"`
-		DirsAfterChroot  []string `json:"dirs_after_chroot"`
-	})
-
-	err = json.Unmarshal(output, resp)
-	require.NoError(t, err)
-
-	assert.Greater(t, len(resp.DirsBeforeChroot), 3)
-	assert.Equal(t, 3, len(resp.DirsAfterChroot))
-}
-
-/*func TestPivotRoot(t *testing.T) {
-	t.Parallel()
-
-	err := syscall.Unshare(syscall.CLONE_NEWNS)
-	require.NoError(t, err)
-
-	newRoot := t.TempDir()
-	oldRoot := "/old_root_fs"
-	oldRoot = filepath.Join(newRoot, oldRoot)
-	err = os.MkdirAll(oldRoot, 0o755)
-	require.NoError(t, err)
-
-	test.PreventSharedPropagationToRootPoint(t, newRoot)
-	test.MakeDirRootPoint(t, newRoot)
-
-	err = syscall.PivotRoot(newRoot, oldRoot)
-	require.NoError(t, err)
-}*/
-
 func TestMount(t *testing.T) {
-	t.Parallel()
-
 	flags := syscall.MS_REC | syscall.MS_BIND | syscall.MS_PRIVATE
 	mountPoint := t.TempDir()
 
@@ -86,9 +46,23 @@ func TestMount(t *testing.T) {
 	assert.ElementsMatch(t, sourceEntries, targetEntries)
 }
 
-func TestRootFSFilesWillBeRevertedAfterUnmountTempFS(t *testing.T) {
-	t.Parallel()
+func TestChroot(t *testing.T) {
+	cmd := exec.Command("./test_data/test_bin_source/chroot", "./test_data/test_fs_source")
+	output, err := cmd.Output()
+	require.NoError(t, err)
 
+	resp := new(struct {
+		DirsBeforeChroot []string `json:"dirs_before_chroot"`
+		DirsAfterChroot  []string `json:"dirs_after_chroot"`
+	})
+
+	err = json.Unmarshal(output, resp)
+	require.NoError(t, err)
+
+	assert.Greater(t, len(resp.DirsBeforeChroot), 3)
+	assert.Equal(t, 3, len(resp.DirsAfterChroot))
+}
+func TestRootFSFilesWillBeRevertedAfterUnmountTempFS(t *testing.T) {
 	mountDir, sourceDir := t.TempDir(), t.TempDir()
 
 	sourceExpectedContentBeforeMount := addTestDirectoriesToDirectory(t, sourceDir, "_source_dir_initial_content")
@@ -114,6 +88,47 @@ func TestRootFSFilesWillBeRevertedAfterUnmountTempFS(t *testing.T) {
 	sourceActualContentAfterUnmount := contentInDirectory(t, sourceDir)
 	assert.ElementsMatch(t, actualContentInDirectoryAfterUnmount, mountPointActualContentBeforeMount)
 	assert.ElementsMatch(t, sourceActualContentAfterUnmount, mountPointExpectedContentAfterMount)
+}
+func TestPivotRoot(t *testing.T) {
+	endpoint1, endpoint2, err := os.Pipe()
+	require.NoError(t, err)
+
+	successMessage := []byte("syscall.PivotRoot performed OK")
+
+	pid, _, errno := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+	require.Equal(t, 0, int(errno))
+
+	isChildProcess := pid == 0
+	if isChildProcess {
+		err = syscall.Unshare(syscall.CLONE_NEWNS)
+		require.NoError(t, err)
+
+		newRoot := t.TempDir()
+		oldRoot := "/old_root_fs"
+		oldRoot = filepath.Join(newRoot, oldRoot)
+		err = os.MkdirAll(oldRoot, 0o755)
+		require.NoError(t, err)
+
+		test.PreventSharedPropagationToRootPoint(t, newRoot)
+		test.MakeDirRootPoint(t, newRoot)
+
+		err = syscall.PivotRoot(newRoot, oldRoot)
+		require.NoError(t, err)
+
+		_, err = endpoint2.Write(successMessage)
+		require.NoError(t, err)
+	} else {
+		err = endpoint2.Close()
+		require.NoError(t, err)
+
+		data, err := io.ReadAll(endpoint1)
+		require.NoError(t, err)
+
+		err = endpoint1.Close()
+		require.NoError(t, err)
+
+		assert.Equal(t, successMessage, data)
+	}
 }
 
 func addTestDirectoriesToDirectory(t *testing.T, dirPath, testDirsNamePostfix string) []string {
