a745296bff0edb313daeba3cb0733ee05a8dc3da
roman <roma.a.erema@gmail.com>
2023-09-16 22:55:54 +0300

    Problem: Merge Two Sorted Lists

---
+++ learning/algorithms/merge_two_sorted_lists/merge_two_sorted_lists_test.go
@@ -0,0 +1,166 @@
+package mergetwosortedlists_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type (
+	ListNode struct {
+		Value int
+		Next  *ListNode
+	}
+	testCase struct {
+		name  string
+		list1 *ListNode
+		list2 *ListNode
+		want  *ListNode
+	}
+)
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+func TestMerge2SortedLists(t *testing.T) {
+	t.Parallel()
+
+	tests := []testCase{
+		normalListsTestCase(),
+		listIsNil(),
+		listHasNegativeNumber(),
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, mergeTwoSortedLists(tt.list1, tt.list2))
+		})
+	}
+}
+
+// Time O(n+m)
+// n = number of nodes in list1
+// m = number of nodes in list2
+//
+// Space O(1)
+// we have a constant space, since we are just shifting the pointers.
+func mergeTwoSortedLists(list1, list2 *ListNode) *ListNode {
+	dummyNode := &ListNode{}
+	tail := dummyNode
+
+	for list1 != nil && list2 != nil {
+		if list1.Value < list2.Value {
+			tail.Next = list1
+			list1 = list1.Next
+		} else {
+			tail.Next = list2
+			list2 = list2.Next
+		}
+
+		tail = tail.Next
+	}
+
+	if list1 != nil {
+		tail.Next = list1
+	}
+
+	if list2 != nil {
+		tail.Next = list2
+	}
+
+	return dummyNode.Next
+}
+
+func normalListsTestCase() testCase {
+	return testCase{
+		name: "Normal lists",
+		list1: &ListNode{
+			Value: 1,
+			Next: &ListNode{
+				Value: 2,
+				Next: &ListNode{
+					Value: 4,
+					Next:  nil,
+				},
+			},
+		},
+		list2: &ListNode{
+			Value: 1,
+			Next: &ListNode{
+				Value: 3,
+				Next: &ListNode{
+					Value: 4,
+					Next:  nil,
+				},
+			},
+		},
+		want: &ListNode{
+			Value: 1,
+			Next: &ListNode{
+				Value: 1,
+				Next: &ListNode{
+					Value: 2,
+					Next: &ListNode{
+						Value: 3,
+						Next: &ListNode{
+							Value: 4,
+							Next: &ListNode{
+								Value: 4,
+								Next:  nil,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func listIsNil() testCase {
+	return testCase{
+		name: "List is nil",
+		list1: &ListNode{
+			Value: 1,
+			Next:  nil,
+		},
+		list2: nil,
+		want: &ListNode{
+			Value: 1,
+			Next:  nil,
+		},
+	}
+}
+
+func listHasNegativeNumber() testCase {
+	return testCase{
+		name: "List has a negative number",
+		list1: &ListNode{
+			Value: -9,
+			Next: &ListNode{
+				Value: 3,
+				Next:  nil,
+			},
+		},
+		list2: &ListNode{
+			Value: 5,
+			Next: &ListNode{
+				Value: 7,
+				Next:  nil,
+			},
+		},
+		want: &ListNode{
+			Value: -9,
+			Next: &ListNode{
+				Value: 3,
+				Next: &ListNode{
+					Value: 5,
+					Next: &ListNode{
+						Value: 7,
+						Next:  nil,
+					},
+				},
+			},
+		},
+	}
+}
