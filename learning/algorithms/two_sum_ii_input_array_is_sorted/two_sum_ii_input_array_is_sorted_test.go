ca56aa2b41ea9c4f91db79fb269d1158c01b5f40
roman <roma.a.erema@gmail.com>
2023-09-16 22:48:34 +0300

    Problem: Two Sum II Input Array Is Sorted

---
+++ learning/algorithms/two_sum_ii_input_array_is_sorted/two_sum_ii_input_array_is_sorted_test.go
@@ -0,0 +1,53 @@
+package twosumiiinputarrayissorted_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		numbers []int
+		target  int
+		want    []int
+	}{
+		{
+			name:    "Simple array",
+			numbers: []int{1, 2, 10, 17},
+			target:  12,
+			want:    []int{2, 3},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, twoSum(tt.numbers, tt.target))
+		})
+	}
+}
+
+// Time O(n), since in the worst case we don't exceed iterations count more then input
+// Space O(1), since we don't involve an additional data structure.
+func twoSum(numbers []int, target int) []int {
+	left, right := 0, len(numbers)-1
+
+	for left <= right {
+		sum := numbers[left] + numbers[right]
+		if sum > target {
+			right--
+		} else if sum < target {
+			left++
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+
+	return nil
+}
