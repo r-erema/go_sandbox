173aab518a205f190197bd208fa464c63f6a7fe2
roman <roma.a.erema@gmail.com>
2023-11-09 08:42:09 +0300

    Problem: Search a 2D Matrix

---
+++ learning/algorithms/search_a_2d_matrix/search_a_2d_matrix_test.go
@@ -0,0 +1,81 @@
+package searcha2dmatrix_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchMatrix(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		matrix [][]int
+		target int
+		want   bool
+	}{
+		{
+			name:   "target exists in matrix",
+			matrix: [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}},
+			target: 3,
+			want:   true,
+		},
+		{
+			name:   "target does not exist in matrix",
+			matrix: [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}},
+			target: 13,
+			want:   false,
+		},
+		{
+			name:   "target does not exist in 1 element matrix",
+			matrix: [][]int{{1}},
+			target: 1,
+			want:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, searchMatrix(tt.matrix, tt.target))
+		})
+	}
+}
+
+// Time O(log(N*M)), since we apply binary search to matrix rows and then apply binary search in the found row
+// Space O(1), we don't involve any additional data structures.
+func searchMatrix(matrix [][]int, target int) bool {
+	var row int
+
+	top, bottom := 0, len(matrix)-1
+
+	for top <= bottom {
+		row = (top + bottom) / 2
+
+		if target > matrix[row][len(matrix[0])-1] {
+			top = row + 1
+		} else if target < matrix[row][0] {
+			bottom = row - 1
+		} else {
+			break
+		}
+	}
+
+	left, right := 0, len(matrix[row])-1
+	for left <= right {
+		pointer := (left + right) / 2
+
+		if matrix[row][pointer] < target {
+			left = pointer + 1
+		} else if matrix[row][pointer] > target {
+			right = pointer - 1
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
