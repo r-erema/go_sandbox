c90189b9b9982c95d8898af3749d9ae7eda4b1d0
roman <roma.a.erema@gmail.com>
2024-01-24 09:07:38 +0300

    Problem: Best Time to Buy and Sell Stock

---
+++ learning/algorithms/best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock_test.go
@@ -0,0 +1,59 @@
+package besttimetobuyandsellstock_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBestTimeToBuyAndSellStock(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		prices []int
+		want   int
+	}{
+		{
+			name:   "Simple set",
+			prices: []int{7, 1, 5, 3, 6, 4},
+			want:   5,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, maxProfit(tt.prices))
+		})
+	}
+}
+
+// Time O(n), since we should iterate all the input
+// Space O(1), sine we don't allocate any additional memory.
+func maxProfit(prices []int) int {
+	buyIndex, sellIndex := 0, 0
+	profit := 0
+
+	for sellIndex < len(prices) {
+		if prices[buyIndex] > prices[sellIndex] {
+			buyIndex = sellIndex
+		}
+
+		profit = max(profit, prices[sellIndex]-prices[buyIndex])
+
+		sellIndex++
+	}
+
+	return profit
+}
+
+func max(n1, n2 int) int {
+	if n1 > n2 {
+		return n1
+	}
+
+	return n2
+}
