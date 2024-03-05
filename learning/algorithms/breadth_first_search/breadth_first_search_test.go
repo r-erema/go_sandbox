1dbbc599fca30d259fe88a37611c6b73570a95d3
roman <roma.a.erema@gmail.com>
2024-03-05 09:21:28 +0300

    Algorithm: Breadth First Search

---
+++ learning/algorithms/breadth_first_search/breadth_first_search_test.go
@@ -0,0 +1,74 @@
+package breadthfirstsearch_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBFS(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name           string
+		edges          [][]int
+		vertices, want []int
+	}{
+		{
+			name: "8 nodes graph",
+			edges: [][]int{
+				0: {1, 2, 3},
+				1: {0, 8},
+				2: {0, 4},
+				3: {0, 6},
+				4: {2, 5, 7},
+				5: {4},
+				6: {3},
+				7: {4},
+				8: {1},
+			},
+			vertices: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			want:     []int{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		},
+		{
+			name: "2 unconnected graphs",
+			edges: [][]int{
+				0: {1, 2},
+				1: {0, 2},
+				2: {0, 1},
+				3: {4, 5},
+				4: {3, 5},
+				5: {3, 4},
+			},
+			vertices: []int{0, 0, 0, 0, 0, 0},
+			want:     []int{1, 1, 1, 0, 0, 0},
+		},
+	}
+
+	for _, tt := range tests {
+		testCase := tt
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			bfs := func(nodeIndex int) {
+				queue := []int{nodeIndex}
+
+				for len(queue) > 0 {
+					nodeIndex, queue = queue[0], queue[1:]
+					testCase.vertices[nodeIndex] = 1
+
+					if nodeIndex < len(testCase.edges) {
+						for _, edge := range testCase.edges[nodeIndex] {
+							if testCase.vertices[edge] == 0 {
+								queue = append(queue, edge)
+							}
+						}
+					}
+				}
+			}
+
+			bfs(0)
+			assert.Equal(t, testCase.want, testCase.vertices)
+		})
+	}
+}
