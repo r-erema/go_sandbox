318b42a2a6d158c3f476979b593c527eb5a2509d
roman <roma.a.erema@gmail.com>
2024-05-25 08:35:57 +0300

    Problem: Climbing Stairs

---
+++ learning/algorithms/climbing_stairs/climbing_stairs_test.go
@@ -0,0 +1,52 @@
+package climbingstairs_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClimbingStairs(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		n, want int
+	}{
+		{
+			name: "2 steps",
+			n:    2,
+			want: 2,
+		},
+		{
+			name: "3 steps",
+			n:    3,
+			want: 3,
+		},
+		{
+			name: "4 steps",
+			n:    4,
+			want: 5,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, climbStairs(tt.n))
+		})
+	}
+}
+
+// Time O(n), since we walk trough the n 1 time
+// Space O(1), we don't use any extra space.
+func climbStairs(n int) int {
+	one, two := 1, 1
+	for i := 0; i < n-1; i++ {
+		one, two = one+two, one
+	}
+
+	return one
+}
