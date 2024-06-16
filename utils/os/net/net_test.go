d396a6b69694ad04958c8b1e48d8633409fe6aa0
roman <roma.a.erema@gmail.com>
2024-06-16 10:51:43 +0300

    Net OS utils

---
+++ utils/os/net/net_test.go
@@ -0,0 +1,23 @@
+package net_test
+
+import (
+	"testing"
+
+	"github.com/r-erema/go_sendbox/utils/os/net"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddVeth(t *testing.T) {
+	t.Parallel()
+
+	t.Cleanup(func() {
+		err := net.DeleteLink("test_veth")
+		require.NoError(t, err)
+	})
+
+	veth, err := net.SetupVeth("test_veth", "10.0.0.1/16", "test_peer", "testNS1")
+	require.NoError(t, err)
+
+	assert.Equal(t, "test_veth", veth.Name)
+}
