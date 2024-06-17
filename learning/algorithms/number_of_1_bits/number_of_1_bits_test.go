68a8a899db42f0245af2656fa9c7a3e502f6b39f
roman <roma.a.erema@gmail.com>
2024-06-17 08:24:23 +0300

    Problem: Number of 1 Bits

---
+++ learning/algorithms/number_of_1_bits/number_of_1_bits_test.go
@@ -0,0 +1,81 @@
+package numberof1bits_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHammingWeight(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		input, want int
+	}{
+		{
+			name:  "3 ones",
+			input: 11,
+			want:  3,
+		},
+		{
+			name:  "1 one",
+			input: 128,
+			want:  1,
+		},
+		{
+			name:  "30 ones",
+			input: 2147483645,
+			want:  30,
+		},
+	}
+
+	for _, tt := range tests {
+		testCase := tt
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, testCase.want, hammingWeight(testCase.input))
+			assert.Equal(t, testCase.want, hammingWeight2(testCase.input))
+			assert.Equal(t, testCase.want, hammingWeight3(testCase.input))
+		})
+	}
+}
+
+// Time O(N), since we iterate input one time
+// Space O(1), since we don't involve any additional data structure.
+func hammingWeight(number int) int {
+	var res int
+
+	for number > 0 {
+		res += number % 2
+		number >>= 1
+	}
+
+	return res
+}
+
+func hammingWeight2(number int) int {
+	var res int
+
+	for number > 0 {
+		number &= number - 1
+		res++
+	}
+
+	return res
+}
+
+func hammingWeight3(number int) int {
+	var res int
+
+	for number > 0 {
+		if number&1 == 1 {
+			res++
+		}
+
+		number >>= 1
+	}
+
+	return res
+}
