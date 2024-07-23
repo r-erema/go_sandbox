d6db22719e51ea0e4e41f7fc2b8832a9eb050c2e
roman <roma.a.erema@gmail.com>
2024-07-23 08:44:09 +0300

    Problem: Search In Rotated Sorted Array

---
+++ learning/algorithms/search_in_rotated_sorted_array/search_in_rotated_sorted_array_test.go
@@ -0,0 +1,84 @@
+package searchinrotatedsortedarray_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchInRotatedSortedArray(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		input  []int
+		target int
+		want   int
+	}{
+		{
+			name:   "target exists",
+			input:  []int{4, 5, 6, 7, 0, 1, 2},
+			target: 0,
+			want:   4,
+		},
+		{
+			name:   "target does not exist",
+			input:  []int{4, 5, 6, 7, 0, 1, 2},
+			target: 3,
+			want:   -1,
+		},
+		{
+			name:   "target does not exist, input with 1 value",
+			input:  []int{1},
+			target: 0,
+			want:   -1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, search(tt.input, tt.target))
+		})
+	}
+}
+
+// Time O(log(N)), since it's binary search
+// Space O(1), sine we don't allocate any additional memory.
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	leftSortedPortion := func(mid int) {
+		if target > nums[mid] || target < nums[left] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	rightSortedPortion := func(mid int) {
+		if target < nums[mid] || target > nums[right] {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if target == nums[mid] {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			leftSortedPortion(mid)
+		} else {
+			rightSortedPortion(mid)
+		}
+	}
+
+	return -1
+}
