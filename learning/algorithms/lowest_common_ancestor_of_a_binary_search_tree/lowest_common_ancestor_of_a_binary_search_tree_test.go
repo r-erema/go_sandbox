530149b99e9c3f292cdb2b140138610324aa91f8
erema <roma.a.erema@gmail.com>
2024-10-23 08:57:05 +0300

    Problem: Lowest Common Ancestor of a Binary Search Tree

---
+++ learning/algorithms/lowest_common_ancestor_of_a_binary_search_tree/lowest_common_ancestor_of_a_binary_search_tree_test.go
@@ -0,0 +1,77 @@
+package lowestcommonancestorofabinarysearchtree_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+// Time O(log n), we don't have to visit every node in the tree, but only 1 node in a level
+// Space O(1), we don't allocate additional memory.
+func TestLowestCommonAncestor(t *testing.T) {
+	t.Parallel()
+
+	commonTree := &TreeNode{
+		Val: 6,
+		Left: &TreeNode{
+			Val:  2,
+			Left: &TreeNode{Val: 0},
+			Right: &TreeNode{
+				Val:   4,
+				Left:  &TreeNode{Val: 3},
+				Right: &TreeNode{Val: 5},
+			},
+		},
+		Right: &TreeNode{
+			Val:   8,
+			Left:  &TreeNode{Val: 7},
+			Right: &TreeNode{Val: 9},
+		},
+	}
+
+	tests := []struct {
+		name string
+		p, q *TreeNode
+		want int
+	}{
+		{
+			name: "lca is root node",
+			p:    &TreeNode{Val: 2},
+			q:    &TreeNode{Val: 8},
+			want: 6,
+		},
+		{
+			name: "lca is node itself",
+			p:    &TreeNode{Val: 2},
+			q:    &TreeNode{Val: 4},
+			want: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tt.want, lowestCommonAncestor(commonTree, tt.p, tt.q).Val)
+		})
+	}
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else {
+			return root
+		}
+	}
+
+	return nil
+}
