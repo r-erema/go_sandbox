e451fef4f9c28901f82a17244c924ebc58dab783
erema <roma.a.erema@gmail.com>
2025-04-02 08:17:08 +0300

    Permutations

---
+++ pkg/socket/server_test.go
@@ -0,0 +1,20 @@
+package socket_test
+
+import (
+	"github.com/r-erema/go_sendbox/pkg/socket"
+	"github.com/r-erema/go_sendbox/pkg/tls"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	t.Parallel()
+
+	clientPubKey, err := tls.Rand32Bytes()
+	require.NoError(t, err)
+
+	resp, err := socket.ClientCall("142.250.150.99", 443, clientPubKey, []byte("Hello from client"))
+	require.NoError(t, err)
+	assert.Equal(t, []byte("Server has got message: Hello from client"), resp)
+}
